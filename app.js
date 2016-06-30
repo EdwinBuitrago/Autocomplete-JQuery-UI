d45f8f0fd771f957b227fc8e6da69bd12b4ee040
EdwinBuitrago <ebuitrago7@hotmail.com>
2016-06-29 19:52:47 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+ar main = function() {
+  $(function(){
+    var cities = [
+      "New York",
+      "Los Angeles",
+      "Miami",
+      "Meriden",
+      "Sacramento",
+      "Utah",
+      "Toronto",
+      "New Jersey",
+      "San Francisco",
+      ];
+      $("#search").autocomplete({
+        source: cities
+      });
+  });
+};  
+
+ 
+$(document).ready(main);
